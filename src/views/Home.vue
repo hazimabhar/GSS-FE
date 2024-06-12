514191bbca89f4ad4b57240c72d56aba08838948
hazimabhar <hazimabhar123@gmail.com>
2024-06-12 16:57:11 +0800

    layout

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex">
+    <div class="w-[15%] min-h-screen bg-slate-500">
+      <div class="flex justify-between p-3 items-center">
+        <div class="flex items-center">
+          <div>logo</div>
+          <div>
+            <div>GSS</div>
+            <div>Grocery Store System</div>
+          </div>
+        </div>
+        <div class="text-right">
+          <i role="button" class="fa-solid fa-chevron-left"></i>
+        </div>
+      </div>
+      <div></div>
+    </div>
+    <div>
+      <div>This is navbar</div>
+      <div>this is content</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {}
+}
+</script>
