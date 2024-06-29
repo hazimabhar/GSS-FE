f7ed616db2502360a6c0fb07810b5835e53c309f
hazimabhar <hazimabhar123@gmail.com>
2024-06-29 19:37:14 +0800

    update login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,45 +1,62 @@
 <template>
   <div class="bg-[#f5f5fa] min-h-screen">
     <div class="flex items-center justify-center">
-      <div class="shadow-lg shodow-inner shadow-[#e6ebef]-500/50 my-[5%] bg-white rounded-[35px]">
-        <div class="py-10">
-          <img src="/login.png" alt="" />
+      <div class="shadow-lg shodow-inner shadow-[#e6ebef]-500/50 my-[8%] py-8 bg-white rounded-[35px] min-w-[40%]">
+        <div class="max-w-[50%]">
+          <div>
+            <img src="/favicon.png" alt="" />
+          </div>
+          <div class="my-8 px-8">
+            <div class="flex text-[30px]">
+              <h1 class="font-bold mr-1">Grocery</h1>
+              <h1>Store</h1>
+            </div>
+            <div class="text-[30px]">
+              <h1 class="font-bold">System</h1>
+            </div>
+          </div>
+          <div class="">
+            <form class="bg-white rounded px-8 pt-6 pb-8">
+              <div class="mb-4">
+                <!-- <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                  Username
+                </label> -->
+                <input
+                  class="shadow appearance-none border border-red-500 rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-blue-200 focus:shadow-outline"
+                  id="emai"
+                  type="email"
+                  placeholder="Please Enter Your Email"
+                />
+                <p class="text-red-500 text-xs italic">Please choose a email.</p>
+              </div>
+              <div class="mb-6">
+                <!-- <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                  Password
+                </label> -->
+                <input
+                  class="shadow appearance-none border border-red-500 rounded w-full py-4 px-3 text-gray-700 mb-3 leading-tight focus:outline-blue-200 focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Please Enter Your Password"
+                />
+                <p class="text-red-500 text-xs italic">Please choose a password.</p>
+              </div>
+              <div class="flex justify-between">
+              <div>
+                <span>Forgot Password?</span>
+              </div>
+              <div>
+                <button>
+                  <i class="fa-solid fa-arrow-right-long"></i>
+                </button>
+              </div>
+            </div>
+            </form>
+          </div>
+          <div class="px-8 text-[15px] font-extralight">
+            <span>hazimabhar's Final Year Project 2022-2023</span>
+          </div>
         </div>
-          
-        <form>
-          <div class="mb-6">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >Email address</label
-            >
-            <input
-              type="email"
-              id="email"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="john.doe@company.com"
-              required
-            />
-          </div>
-          <div class="mb-6">
-            <label
-              for="password"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >Password</label
-            >
-            <input
-              type="password"
-              id="password"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="•••••••••"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
-          </button>
-        </form>
         <!-- <router-link :to="{ name: 'Dashboard' }">Login</router-link> -->
       </div>
     </div>
