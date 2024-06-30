d3bf6220967601f01706c60687bed9ba17e8334f
hazimabhar <hazimabhar123@gmail.com>
2024-06-30 15:14:12 +0800

    update login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,11 +2,11 @@
   <div class="bg-[#f5f5fa] min-h-screen">
     <div class="flex items-center justify-center">
       <div class="shadow-lg shodow-inner shadow-[#e6ebef]-500/50 my-[8%] py-8 bg-white rounded-[35px] min-w-[40%]">
-        <div class="max-w-[50%]">
-          <div>
-            <img src="/favicon.png" alt="" />
+        <div class="max-w-[56%]">
+          <div class="px-8 ">
+            <img src="/login.png" alt="" />
           </div>
-          <div class="my-8 px-8">
+          <div class="my-4 px-12">
             <div class="flex text-[30px]">
               <h1 class="font-bold mr-1">Grocery</h1>
               <h1>Store</h1>
@@ -16,13 +16,13 @@
             </div>
           </div>
           <div class="">
-            <form class="bg-white rounded px-8 pt-6 pb-8">
+            <form class="bg-white rounded px-12 pt-6 pb-8">
               <div class="mb-4">
                 <!-- <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                   Username
                 </label> -->
                 <input
-                  class="shadow appearance-none border border-red-500 rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-blue-200 focus:shadow-outline"
+                  class="shadow appearance-none border border-red-500 rounded w-full py-4 px-3 mb-3 text-gray-700 leading-tight focus:outline-blue-200 focus:shadow-outline"
                   id="emai"
                   type="email"
                   placeholder="Please Enter Your Email"
@@ -41,11 +41,11 @@
                 />
                 <p class="text-red-500 text-xs italic">Please choose a password.</p>
               </div>
-              <div class="flex justify-between">
-              <div>
+              <div class="flex justify-between items-center">
+              <div class="text-sm text-[#09a0fe] font-medium">
                 <span>Forgot Password?</span>
               </div>
-              <div>
+              <div class="bg-[#09a0fe] text-white py-2 px-8 rounded-md ">
                 <button>
                   <i class="fa-solid fa-arrow-right-long"></i>
                 </button>
@@ -53,7 +53,7 @@
             </div>
             </form>
           </div>
-          <div class="px-8 text-[15px] font-extralight">
+          <div class="px-12 text-[11px] font-medium">
             <span>hazimabhar's Final Year Project 2022-2023</span>
           </div>
         </div>
